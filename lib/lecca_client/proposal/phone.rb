088ef65fcc2b99388c523519d1cc3a6986d8071b
Rafael Izidoro <izidoro.rafa@gmail.com>
2016-07-27 20:22:55 -0300

    add phone part

--- lib/lecca_client/proposal/phone.rb
+++ lib/lecca_client/proposal/phone.rb
@@ -1,35 +1,22 @@
 module LeccaClient
   module Proposal
     class Phone < Hashie::Dash
-      property :codigo_produto, required: true
-      property :data_operacao
-      property :codigo_agencia
-      property :codigo_rede
-      property :digito_rede
-      property :codigo_loja
-      property :digito_loja
-      property :nome_loja
-      property :numero_operacao
-      property :codigo_tabelo_juros
-      property :taxa_juros
-      property :data_primeira_parcela
-      property :valor_compra
-      property :valor_entrada
-      property :valor_principal
-      property :valor_cac
-      property :quantidade_parcela
-      property :valor_parcela
-      property :valor_ioc
-      property :forma_liberacao
-      property :forma_liquidacao
-      property :digito_agencia
-      property :digito_conta
-      property :nome_favorecido
-      property :praca_doc
-      property :banco
+      include LeccaClient::Utils
+
+      property :ddd_telefone, required: true
+      property :telefone, required: true
+      property :celular
+      property :email
+      property :ddd_celular
 
       def build
-        '1'
+        parts = ''
+
+        parts << justify_number(ddd_telefone, 4)
+        parts << justify_number(telefone, 12)
+        parts << justify_number(celular, 12)
+        parts << justify_string(email, 50)
+        parts << justify_number(ddd_celular, 4)
       end
     end
   end

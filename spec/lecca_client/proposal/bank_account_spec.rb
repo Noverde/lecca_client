18c469fac8d89a6158fc639982aa5c3c676cc9e9
Rafael Izidoro <izidoro.rafa@gmail.com>
2016-07-28 10:07:45 -0300

    create spec for bank_account required field condition

--- spec/lecca_client/proposal/bank_account_spec.rb
+++ spec/lecca_client/proposal/bank_account_spec.rb
@@ -6,6 +6,25 @@ describe LeccaClient::Proposal::BankAccount do
 
   let(:bank_account_hash) { ProposalMock.bank_account }
 
+  context 'when product code is CP' do
+    before do
+      allow(LeccaClient).to receive_message_chain(:config, :proposal).and_return({ codigo_produto: 'CP' })
+      bank_account_hash[:digito_agencia] = nil
+    end
+
+    it 'returns error if required field are not filled' do
+      expect { bank_account.build }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when product code is not CP' do
+    it 'does not require fields to be filled' do
+      bank_account_hash[:digito_agencia] = nil
+
+      expect { bank_account.build }.not_to raise_error(ArgumentError)
+    end
+  end
+
   describe '#build' do
     let(:line) { '07                     1FERNANDA LINS DA COSTA             000000000000001001309700021464001001000000000' }
 
